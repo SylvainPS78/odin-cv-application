0a0f0541c0a3bfca2c5fed2a3ab41d800f913933
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-08-13 14:25:34 +0700

    FEAT Create proj. structure, component, and asset

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,38 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>
+          &copy; <time dateTime="2025">2025</time> Created by{" "}
+          <a
+            href="https://github.com/SylvainPS78"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sylvain Web
+          </a>
+        </p>
+        <p>
+          Photo by{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://unsplash.com/@miikola"
+          >
+            Nicolas I.
+          </a>{" "}
+          on{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://unsplash.com/photos/person-holding-dslr-camera-azPNN8Rcr48"
+          >
+            Unsplash
+          </a>
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
