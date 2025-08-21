9ab40946ced109ab56e475ce673b91f2b7e83096
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-08-21 10:10:34 +0700

    FEAT Start CVPreview aside (skills done)

--- src/assets/components/CVPreviem.jsx
+++ src/assets/components/CVPreviem.jsx
@@ -1,14 +1,97 @@
 import React from "react";
+import defaultUserPicture from "../../assets/img/default_user_picture.jpg";
 
 const CVPreview = ({ formData }) => {
   return (
-    <div className="cv-preview-container">
-      <h2 className="user-first-name">{formData?.firstName || ""}</h2>
-      <h2 className="user-last-name">{formData?.lastName || ""}</h2>
-      {formData?.skillDescription?.list?.map((skill, index) => (
-        <p key={index} className="user-skill">{skill}</p>
-      ))}
-    </div>
+    <>
+      <div className="cv-preview-container">
+        <div className="aside-preview-container">
+          <img
+            className="user-picture-preview"
+            src={defaultUserPicture}
+            alt="User profile picture"
+            width="80px"
+            height="80px"
+          />
+          <h4 className="aside-preview-section-title">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#242940"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              aria-hidden="true"
+              className="user-icon"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+            <span>About Me</span>
+          </h4>
+
+          <h4 className="aside-preview-section-title">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#242940"
+              aria-hidden="true"
+              className="contact-icon"
+            >
+              <path d="M200-246q54-53 125.5-83.5T480-360q83 0 154.5 30.5T760-246v-514H200v514Zm280-194q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm69-80h422q-44-39-99.5-59.5T480-280q-56 0-112.5 20.5T269-200Zm211-320q-25 0-42.5-17.5T420-580q0-25 17.5-42.5T480-640q25 0 42.5 17.5T540-580q0 25-17.5 42.5T480-520Zm0 17Z" />
+            </svg>
+            <span>Contact</span>
+          </h4>
+
+          <h4 className="aside-preview-section-title">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#242940"
+              aria-hidden="true"
+              className="skills-icon"
+            >
+              <path d="M240-80v-172q-57-52-88.5-121.5T120-520q0-150 105-255t255-105q125 0 221.5 73.5T827-615l52 205q5 19-7 34.5T840-360h-80v120q0 33-23.5 56.5T680-160h-80v80h-80v-160h160v-200h108l-38-155q-23-91-98-148t-172-57q-116 0-198 81t-82 197q0 60 24.5 114t69.5 96l26 24v208h-80Zm254-360Zm-54 80h80l6-50q8-3 14.5-7t11.5-9l46 20 40-68-40-30q2-8 2-16t-2-16l40-30-40-68-46 20q-5-5-11.5-9t-14.5-7l-6-50h-80l-6 50q-8 3-14.5 7t-11.5 9l-46-20-40 68 40 30q-2 8-2 16t2 16l-40 30 40 68 46-20q5 5 11.5 9t14.5 7l6 50Zm40-100q-25 0-42.5-17.5T420-520q0-25 17.5-42.5T480-580q25 0 42.5 17.5T540-520q0 25-17.5 42.5T480-460Z" />
+            </svg>
+            <span>Skills</span>
+          </h4>
+          <div className="aside-preview-section-content">
+            {" "}
+            {formData?.skillDescription?.list?.map((skill, index) => (
+              <p key={index} className="user-skill">
+                {skill}
+              </p>
+            ))}
+          </div>
+
+          <h4 className="aside-preview-section-title">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#242940"
+              aria-hidden="true"
+              className="language-icon"
+            >
+              <path d="M620-40q-104 0-183.5-62T331-260q45 2 89-9t84-31h164q1-11 1.5-21.5t.5-21.5q0-9-.5-18.5T668-380h-59q16-18 29.5-38t24.5-42h141q-20-30-48-52.5T693-547q5-20 6.5-41t.5-41q96 26 158 105.5T920-340q0 125-87.5 212.5T620-40Zm-95-102q-7-20-12.5-39t-9.5-39h-67q17 25 39.5 45t49.5 33Zm95 14q12-22 20.5-45t14.5-47h-70q6 24 15 47t20 45Zm95-14q27-13 49.5-33t39.5-45h-67q-5 20-10 39t-12 39Zm33-158h88q2-10 3-19.5t1-20.5q0-11-1-20.5t-3-19.5h-88q1 9 1.5 18.5t.5 18.5q0 11-.5 21.5T748-300Zm-408-20q-125 0-212.5-87.5T40-620q0-125 87.5-212.5T340-920q125 0 212.5 87.5T640-620q0 125-87.5 212.5T340-320Zm0-80q91 0 155.5-64.5T560-620q0-91-64.5-155.5T340-840q-91 0-155.5 64.5T120-620q0 91 64.5 155.5T340-400ZM240-640q17 0 28.5-11.5T280-680q0-17-11.5-28.5T240-720q-17 0-28.5 11.5T200-680q0 17 11.5 28.5T240-640Zm100 176q48 0 85.5-27t54.5-69H200q17 42 54.5 69t85.5 27Zm100-176q17 0 28.5-11.5T480-680q0-17-11.5-28.5T440-720q-17 0-28.5 11.5T400-680q0 17 11.5 28.5T440-640Zm-100 20Z" />
+            </svg>
+            <span>language</span>
+          </h4>
+        </div>
+        <div className="main-preview-container">
+          <h2 className="user-first-name">{formData?.firstName || ""}</h2>
+          <h2 className="user-last-name">{formData?.lastName || ""}</h2>
+        </div>
+      </div>
+    </>
   );
 };
 
